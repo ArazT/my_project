78d19f05990438554442af6d1ede19321314845a
ArazT <tarverdiyev85@inbox.ru>
2021-09-09 12:07:54 +0400

    versiya3

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            for (; i < 9; i++)
+            {
+                Console.WriteLine($"{i} - {i * i}");
+
+                Console.ReadKey();
+            }
+        }
+        
+
+    } 
+}
